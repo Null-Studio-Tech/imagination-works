aa314cd0b1ebca2fb860739aa221495910bde9e6
dongzhigang <dongzhigang@megvii.com>
2023-10-27 20:22:34 +0800

    feat: 完成动画框架

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,15 @@ import { detectWebgl } from './detect-webgl';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
-import './style.css';
-import ScrollSmoother from 'gsap-trial/ScrollSmoother';
-import { ScrollTrigger } from 'gsap-trial/ScrollTrigger';
-import { gsap } from 'gsap-trial';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { OutputPass } from 'three/examples/jsm/postprocessing/OutputPass.js';
-import { BloomPass } from 'three/examples/jsm/postprocessing/BloomPass.js';
-import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass.js';
-import { DotScreenPass } from 'three/examples/jsm/postprocessing/DotScreenPass.js';
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
-import { ClearMaskPass, MaskPass } from 'three/examples/jsm/postprocessing/MaskPass.js';
-import { BleachBypassShader } from 'three/examples/jsm/shaders/BleachBypassShader.js';
-import { SepiaShader } from 'three/examples/jsm/shaders/SepiaShader.js';
-import { VignetteShader } from 'three/examples/jsm/shaders/VignetteShader.js';
-import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader.js';
-import { HorizontalBlurShader } from 'three/examples/jsm/shaders/HorizontalBlurShader.js';
-import { VerticalBlurShader } from 'three/examples/jsm/shaders/VerticalBlurShader.js';
-import { ColorifyShader } from 'three/examples/jsm/shaders/ColorifyShader.js';
+
+import { ScrollSmoother } from 'gsap-trial/ScrollSmoother';
+import { ScrollTrigger } from 'gsap-trial/ScrollTrigger';
+import { gsap } from 'gsap-trial';
+
+import './style.css';
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
@@ -39,7 +29,9 @@ const createRender = () => {
   render.toneMapping = THREE.ACESFilmicToneMapping;
   render.toneMappingExposure = 1;
   window.addEventListener('resize', () => {
+    console.log('render size', renderConfig.RENDER_WIDTH, renderConfig.RENDER_HEIGHT)
     render.setSize(renderConfig.RENDER_WIDTH, renderConfig.RENDER_HEIGHT)
+    render.setPixelRatio(window.devicePixelRatio);
   });
   return render;
 }
@@ -52,26 +44,17 @@ const createScene = () => {
 
 const createCamera = (camera?: THREE.PerspectiveCamera) => {
   const _camera = camera ? camera : new THREE.PerspectiveCamera(35, renderConfig.RENDER_ASPECT, 0.1, 2000);
-  window.addEventListener('resize', () => {
-    _camera.aspect = renderConfig.RENDER_ASPECT;
-    _camera.updateMatrix();
-  });
   return _camera;
 }
 
 const createOrbitControls = (render: THREE.WebGLRenderer, camera: THREE.Camera) => {
   const controls = new OrbitControls(camera, render.domElement);
-  // controls.enableDamping = true;
+  controls.enableDamping = true;
   controls.minDistance = 0;
   controls.maxDistance = 3000;
   return controls;
 }
 
-const animate = (callback: () => void) => {
-  const cb = callback;
-  requestAnimationFrame(() => { cb() });
-  cb();
-}
 
 // 渲染流程
 const init = () => {
@@ -81,8 +64,8 @@ const init = () => {
   const renderer = createRender();
   appContainer.appendChild(renderer.domElement);
   const scene = createScene();
+  // 初始化相机
   let camera = createCamera();
-
   camera.position.set(0, 0, -35);
 
   //创建光源
@@ -90,10 +73,8 @@ const init = () => {
   const lightB = new THREE.PointLight(0xFF9271, 0.1, 3.1);
   const lightC = new THREE.PointLight(0xFFE0DA, 4, 3.1);
 
-  // const environment = new RoomEnvironment(renderer);
-  // const pmremGenerator = new THREE.PMREMGenerator(renderer);
-  // scene.environment = pmremGenerator.fromScene(environment).texture;
 
+  // 需要控制相机视角，可以放开注释
   // const controls = createOrbitControls(renderer, camera);
 
   // controls.addEventListener('change', () => {
@@ -101,23 +82,11 @@ const init = () => {
   //   // console.log(camera.position);
   // })
 
-  const contentHeight = document.querySelector<HTMLDivElement>('#content')?.offsetHeight || 2048;
-  const maxOffset = contentHeight - window.innerHeight;
-  const a = (1 - 0) / (maxOffset - 0);
-  const b = 1 - a * maxOffset;
-
-
-  const controls = createOrbitControls(renderer, camera);
-  controls.addEventListener('change', () => {
-    console.log(camera.position)
-  })
-
-
-  const clocker = new THREE.Clock();
-
+  // 加载模型文件
   new GLTFLoader().setPath('models/').load('myhome_export.gltf', (glft) => {
     console.log(glft);
 
+    // 加灯光
     const light1 = glft.scene.getObjectByName('lightProxy_01') as THREE.Mesh;
     lightA.position.copy(light1.position);
     glft.scene.remove(light1);
@@ -134,162 +103,96 @@ const init = () => {
     glft.scene.add(lightC);
 
     scene.add(glft.scene);
-    // const target = glft.scene.getObjectByName('Object_3');
-    // const target = glft.scene;
-    // console.log(target);
-    // target?.children.forEach((mesh) => {
-    //   if (mesh?.isMesh === true) {
-    //     const pointsMesh = new THREE.Points((mesh as THREE.Mesh).geometry, (mesh as THREE.Mesh).material);
-    //     scene.add(pointsMesh);
-    //   }
-    // })
-    // glft.scene.children.forEach((mesh) => {
-    //   // scene.add(mesh);
-    //   const pointsMesh = new THREE.Points((mesh as THREE.Mesh).geometry, (mesh as THREE.Mesh).material);
-    //   scene.add(pointsMesh);
-    // })
-    // console.log(glft.scene.animations[0].uuid);
+
+    // 使用模型中的相机替换当前相机
     const cameraObj = createCamera(glft.cameras[0] as THREE.PerspectiveCamera);
     cameraObj.aspect = renderConfig.RENDER_ASPECT;
-    cameraObj.updateMatrix();
+    cameraObj.updateProjectionMatrix();
+    window.addEventListener('resize', () => {
+      cameraObj.aspect = renderConfig.RENDER_ASPECT;
+      cameraObj.updateProjectionMatrix();
+    });
+
+
+    // 创建动画mixer 相机和椅子分开，动画要作用在相机对象上
     let mixer: THREE.AnimationMixer;
     let cameraMixer: THREE.AnimationMixer;
-    let cameraAction: THREE.AnimationAction;
-    // let cameraAction:THREE.AnimationAction;
     if (glft.animations.length > 0) {
       mixer = new THREE.AnimationMixer(glft.scene);
       cameraMixer = new THREE.AnimationMixer(cameraObj);
-      cameraAction = cameraMixer.clipAction(glft.animations[0]).play();
+      cameraMixer.clipAction(glft.animations[0]).play();
       mixer.clipAction(glft.animations[1]).play();
       mixer.clipAction(glft.animations[2]).play();
     }
     renderer.render(scene, cameraObj);
 
 
+    // 后期滤镜处理
     const renderScene = new RenderPass(scene, cameraObj);
-
     const bloomPass = new UnrealBloomPass(new THREE.Vector2(renderConfig.RENDER_WIDTH, renderConfig.RENDER_HEIGHT), 0.7, 0.2, 0.5);
     bloomPass.strength = 0.15;
     bloomPass.radius = 0.2;
     bloomPass.threshold = 0.8;
-
     const outputPass = new OutputPass();
-
-
-    //--------
-
-    //
-
-    const shaderBleach = BleachBypassShader;
-    const shaderSepia = SepiaShader;
-    const shaderVignette = VignetteShader;
-
-    const effectBleach = new ShaderPass(shaderBleach);
-    const effectSepia = new ShaderPass(shaderSepia);
-    const effectVignette = new ShaderPass(shaderVignette);
-    const gammaCorrection = new ShaderPass(GammaCorrectionShader);
-
-    effectBleach.uniforms['opacity'].value = 0.95;
-
-    effectSepia.uniforms['amount'].value = 0.9;
-
-    effectVignette.uniforms['offset'].value = 0.95;
-    effectVignette.uniforms['darkness'].value = 1.6;
-
-    const effectBloom = new BloomPass(0.5);
-    const effectFilm = new FilmPass(0.35);
-    const effectFilmBW = new FilmPass(0.35, true);
-    const effectDotScreen = new DotScreenPass(new THREE.Vector2(0, 0), 0.5, 0.8);
-
-    const effectHBlur = new ShaderPass(HorizontalBlurShader);
-    const effectVBlur = new ShaderPass(VerticalBlurShader);
-    effectHBlur.uniforms['h'].value = 2 / (renderConfig.RENDER_WIDTH / 2);
-    effectVBlur.uniforms['v'].value = 2 / (renderConfig.RENDER_HEIGHT / 2);
-
-    const effectColorify1 = new ShaderPass(ColorifyShader);
-    const effectColorify2 = new ShaderPass(ColorifyShader);
-    effectColorify1.uniforms['color'] = new THREE.Uniform(new THREE.Color(1, 0.8, 0.8));
-    effectColorify2.uniforms['color'] = new THREE.Uniform(new THREE.Color(1, 0.75, 0.5));
-
-    const clearMask = new ClearMaskPass();
-    const renderMask = new MaskPass(scene, cameraObj);
-    const renderMaskInverse = new MaskPass(scene, cameraObj);
-
-    renderMaskInverse.inverse = true;
-
-    //
-
-    const rtParameters = {
-      stencilBuffer: true
-    };
-
-    const rtWidth = renderConfig.RENDER_WIDTH / 2;
-    const rtHeight = renderConfig.RENDER_HEIGHT / 2;
-
-    //
-
-    const renderModel = new RenderPass(scene, cameraObj);
-
-    renderModel.clear = false;
-
-    // const composerScene = new EffectComposer(renderer, new THREE.WebGLRenderTarget(rtWidth * 2, rtHeight * 2, rtParameters));
-
-    // composerScene.addPass(renderBackground);
-    // composerScene.addPass(renderModel);
-    // composerScene.addPass(renderMaskInverse);
-    // composerScene.addPass(effectHBlur);
-    // composerScene.addPass(effectVBlur);
-    // composerScene.addPass(clearMask);
-    //--------
-
     const composer = new EffectComposer(renderer);
     composer.addPass(renderScene);
     composer.addPass(bloomPass);
     composer.addPass(outputPass);
 
-    // composer.addPass(gammaCorrection);
-    // composer.addPass(effectSepia);
-    // composer.addPass(effectFilm);
-    // composer.addPass(effectVignette);
 
-
-    // cameraAction.getClip().duration
-    // const duration = glft.animations[0].duration;
-    // const frames = glft.animations[0].tracks.length;
-
-
-    let progress = 0;
     ScrollSmoother.create({
       wrapper: '#app',
       content: '#content',
       smooth: 0.5,
-      onUpdate: self => {
-        // console.log('progress', progress);
-        // progress = a * self.offset(self.wrapper()) + b
-        // // const delta = duration / frames * progress;
-        // const delta = 0.01;
-        // console.log('delta', delta);
-        // mixer.update(delta);
-
-        // cameraMixer.update(delta);
-        // renderer.render(scene, cameraObj);
+      ignoreMobileResize: true,
+      normalizeScroll: true,
+      onUpdate: (self) => {
+        console.log(self.progress);
+        mixer.setTime(5.999 * self.progress);
+        cameraMixer.setTime(5.999 * self.progress);
+        renderer.render(scene, cameraObj);
+        composer.render();
       }
     })
 
-    const animate = () => {
+    // const tl = gsap.timeline({
+    //   scrollTrigger:{
+    //     trigger:'.scene',
+    //     scrub:true,
+    //     pin:true,
+    //     start: "top top",
+    //     end: "+=100%",
+    //   }
+    // })
 
-      requestAnimationFrame(animate);
+    gsap.from('#logo', { x: -999,duration:2 })
 
-      const delta = clocker.getDelta();
-      mixer.update(delta);
-      cameraMixer.update(delta);
-      renderer.render(scene, cameraObj);
-      composer.render(delta);
-      // console.log(clocker.getDelta());
+    gsap.utils.toArray('.scene').forEach((sceneEle) => {
+      gsap.from((sceneEle as HTMLDivElement).querySelector('section'), {
+        scrollTrigger: {
+          trigger: sceneEle as HTMLDivElement,
+          scrub: true,
+          pin: true,
+          start: "top top",
+          end: "+=100%",
+        },
+        // x: -999,
+        scale: 0.5,
+        ease: 'power2'
+      })
+    })
 
-    }
-    animate();
-
+    // 自动播放
+    // const clocker = new THREE.Clock();
+    // const animate = () => {
+    //   requestAnimationFrame(animate);
+    //   const delta = clocker.getDelta();
+    //   mixer.update(delta);
+    //   cameraMixer.update(delta);
+    //   renderer.render(scene, cameraObj);
+    //   composer.render(delta);
+    // }
+    // animate();
   })
 
 }
